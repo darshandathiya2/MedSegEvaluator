28b8be3cb3449398be24d5e056384c57ad4ccef0
Darshan  Dathiya <64348663+darshandathiya2@users.noreply.github.com>
2025-11-24 15:57:18 +0530

    Update project name to 'MedDegEvaluator'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'medsegevaluator'
+project = 'MedDegEvaluator'
 author = 'Darshan Dathiya'
 version = '0.1.0'
 release = '0.1.0'
