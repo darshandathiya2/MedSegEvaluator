680e8723ed3c465cfd4ca96ae4f8f77335e0c064
Darshan  Dathiya <64348663+darshandathiya2@users.noreply.github.com>
2025-12-02 14:45:49 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-#sys.path.insert(0, os.path.abspath('../medsegevaluator'))
-# Path to src folder inside medsegevaluator
-sys.path.insert(0, os.path.abspath('../../medsegevaluator/src'))
 
 # -- Project information -----------------------------------------------------
 
