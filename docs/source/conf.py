da592d6b2d83b06b85edd76ab85fe1cfe16a78de
Darshan  Dathiya <64348663+darshandathiya2@users.noreply.github.com>
2026-01-14 12:46:41 +0530

    Add 'datetime', 'typing', and 'tqdm' to autodoc mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ autodoc_default_options = {
 autodoc_mock_imports = [
     'tensorflow', 'keras', 'cv2', 'numpy', 'pandas',
     'nibabel', 'pydicom', 'matplotlib', 'scipy', 'plotly',
-     'seaborn' , 'skimage' 
+     'seaborn' , 'skimage', 'datetime', 'typing', 'tqdm'
 ]
 
 # -- Options for HTML output -------------------------------------------------
